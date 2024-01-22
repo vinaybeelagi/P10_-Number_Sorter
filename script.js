ed93fd8d2267508a2bccb72fe1f938b66b1451a2
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-22 14:38:43 +0530

    Implement the bubbleSort function

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ const updateUI = (array = []) => {
 }
 
 sortButton.addEventListener("click", sortInputArray);
+
+//  Implement the bubbleSort function
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+  
+    return array;
+  }
+  
