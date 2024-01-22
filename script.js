cbfa19d6888d1da7bc732abfed1b051d5f367d60
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-22 14:43:05 +0530

    Implement the insertionSort function

--- script.js
+++ script.js
@@ -49,4 +49,20 @@ const selectionSort = (array) => {
   
     return array;
   }
+  // Implement the insertionSort function
+const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+      const currValue = array[i];
+      let j = i - 1;
+  
+      while (j >= 0 && array[j] > currValue) {
+        array[j + 1] = array[j];
+        j--;
+      }
+      array[j + 1] = currValue;
+    }
+  
+    return array;
+  }
+  
   
